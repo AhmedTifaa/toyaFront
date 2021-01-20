f474185a21258d6302d9a858b4b13fcdbde12356
affsquare <ahmedtifaa53@gmail.com>
2021-01-20 10:33:31 +0200

    upload project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductsPageComponent } from './components/products-page/products-page.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsPageComponent,
+    ProductDetailsComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
